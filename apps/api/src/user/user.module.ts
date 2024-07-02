ad83589c794ad708f6ceaff582cd4096ec183338
akash-skillstreet <akash.g@skillstreet.io>
2024-07-02 14:32:20 +0530

    circular dependency confilct

--- apps/api/src/user/user.module.ts
+++ apps/api/src/user/user.module.ts
@@ -1,21 +1,14 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UserController } from './controllers';
 import { UserService } from './services';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities';
 import { AbilityModule } from '../ability/ability.module';
 import { PassportModule } from '@nestjs/passport';
-import { AuthModule } from '../auth/auth.module';
 import { MailModule } from '../mail/mail.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([User]),
-    forwardRef(() => AuthModule),
-    AbilityModule,
-    PassportModule,
-    MailModule,
-  ],
+  imports: [TypeOrmModule.forFeature([User]), AbilityModule, PassportModule, MailModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
