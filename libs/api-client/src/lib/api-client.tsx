33576c6b86f961696d2a4c378fbf565d6b033afc
akashgirme <akash17g@gmail.com>
2024-07-28 04:25:54 +0000

    RTK-Query Generation

---
+++ libs/api-client/src/lib/api-client.tsx
@@ -0,0 +1,15 @@
+import styled from 'tailwind';
+
+const StyledApiClient = styled.div`
+  color: pink;
+`;
+
+export function ApiClient() {
+  return (
+    <StyledApiClient>
+      <h1>Welcome to ApiClient!</h1>
+    </StyledApiClient>
+  );
+}
+
+export default ApiClient;
