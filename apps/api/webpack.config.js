f07b1c45bf66ef8f21010d3a188733e913dc1af9
akashgirme <akash17g@gmail.com>
2024-08-29 16:02:58 +0000

    docker file changes and serving react build issue fix

--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -12,17 +12,8 @@ module.exports = {
       main: './src/main.ts',
       tsConfig: './tsconfig.app.json',
       assets: ['./src/assets'],
-      optimization: process.env['NODE_ENV'] === 'production' ? 'all' : 'none',
+      optimization: 'all',
       outputHashing: process.env['NODE_ENV'] === 'production',
     }),
   ],
 };
-
-// const { composePlugins, withNx } = require('@nx/webpack');
-
-// // Nx plugins for webpack.
-// module.exports = composePlugins(withNx(), (config) => {
-//   // Update the webpack config as needed here.
-//   // e.g. `config.plugins.push(new MyPlugin())`
-//   return config;
-// });
